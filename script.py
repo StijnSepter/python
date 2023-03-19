394ee8004ed61e60fe5daaa5007f60aafb4342a4
Stijn <113425138+StijnSepter@users.noreply.github.com>
2023-03-19 22:21:31 +0100

    Add files via upload

---
+++ script.py
@@ -0,0 +1,94 @@
+# age = 20
+# first_name = "stijn"
+# is_online = False
+
+# # print(age)
+# print(first_name)
+# print(is_online)
+
+# name = input("hello ")
+# print("hello " + name)
+
+# brith_year = input("what is your brith year? ")
+# age = 2023 - int(brith_year)
+
+# print(age)
+
+# first = float(input("write here a number "))
+# second = float(input("write here a  second number "))
+# sum = first + second
+# print ("the anwser: " + str(sum))
+
+# course = "this is a test"
+# print (course.replace('a', 'maakt niet uit'))
+# print ('is' in course)
+
+# price = 25
+# print(price > 25 and price < 40)  #? als ze bijde klopen = true
+# print(price > 32 or price < 100)  #? als er 1 klopt = true
+# ? als het klopt klopt het niet dus = flase als het niet klopt = true
+# print(not price > 26)
+
+
+# temp = 5
+# if temp < 25:
+#     print("het is een warme dag")
+# elif temp > 15:
+#     print("het is best fris buiten maar wel mooi weer")
+# elif temp > 10:
+#     print("het is best koud")
+# if temp > 25:
+#     print("het is best wel fris buiten")
+# else:
+#     print("het is een slechte dag")
+# print("het is een mooie dag")
+
+
+# w = int(input("weight: "))
+# u = input("(K)g of (L)bs: ")
+# if u == 'k' or 'K':
+#     converted = w / 0.45
+#     print(str(converted))
+# else:
+#     converted = w * 0.45
+#     print(str(converted))
+
+
+# ! pas op dit kan verkeert gaan!
+# i = 1
+# while i <= 1_000:
+#     print(i * '*')
+#     i += 1
+
+
+# names = ["stijn", "bob", "roos", "filloniea"]
+# print(names[0])
+# print(names[-2])
+# print(names[0:4])
+
+# numbers = [1, 2, 3, 4, 5,]
+# print(numbers)
+# numbers.append(6)
+# numbers.insert(3, 4)
+# print(numbers)
+# numbers.remove(2)
+# print(numbers)
+# print(1 in numbers)
+# * print(numbers.clear())
+# print(numbers)
+
+# numbers = [1, 2, 3, 4, 5,]
+# for item in numbers:
+#     print(item)
+
+# i = 0
+# while i < len(numbers):
+#     print((numbers[i]))
+#     i += 1
+
+# numbers = range(10, 20, 2) # je hebt deze rij niet nodig als je range in de regel hier onder gebruikt
+# for number in range(5):
+#     print(number)
+
+# ? https://www.youtube.com/watch?v=kqtD5dpn9C8
+# ? dit is de link naar de tutorial die ik heb gebruikt om de bassis van python the leren
